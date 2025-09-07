a04793e91d95697964ae765ccf97dea807bdc8a3
Asep Dwi Saputra <akunasep111@gmail.com>
2025-09-07 15:43:00 +0700

    modif pemesanan-mengembalikan stock yang sudah dipesan +1

--- application/controllers/Pemesanan.php
+++ application/controllers/Pemesanan.php
@@ -33,9 +33,26 @@ class Pemesanan extends CI_Controller
 
     public function tolak($id_pemesanan)
     {
+        // Ambil data pemesanan dulu untuk tahu id_buku
+        $pemesanan = $this->db->where('id_pemesanan', $id_pemesanan)
+            ->get('pemesanan')
+            ->row();
+
+        if ($pemesanan) {
+            $id_buku = $pemesanan->id_buku;
+
+            // Tambahkan stock buku +1
+            $this->db->set('stock', 'stock+1', FALSE);
+            $this->db->where('id_buku', $id_buku);
+            $this->db->update('buku');
+        }
+
+        // Panggil model untuk update status pemesanan menjadi ditolak
         $this->Pemesanan_model->tolak_pemesanan($id_pemesanan);
+
         redirect('pemesanan');
     }
+
 
     public function pesan()
     {
